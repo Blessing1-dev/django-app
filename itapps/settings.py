249b795c7389955027bbe2d349d1584e397f678b
Blessing1-dev <c4046182@hallam.shu.ac.uk>
2025-04-29 02:16:14 +0100

    update

--- itapps/settings.py
+++ itapps/settings.py
@@ -162,14 +162,6 @@ LOGIN_REDIRECT_URL = 'itreporting:home'
 LOGIN_URL = 'login' 
 
 # Email backend
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend' 
 EMAIL_FILE_PATH = BASE_DIR / 'emails' 
-
-# SMTP server details
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-# Your Gmail login
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')   
+  
